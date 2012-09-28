b7625c481e7abfe62cbfd2a226b55cb6ff155c2f
Tom <bballts14@gmx.de>
2012-09-28 13:53:02 +0200

    Connect to DB on app load.

--- eve-web.rb
+++ eve-web.rb
@@ -1,5 +1,18 @@
-require 'sinatra'
-require 'slim'
+%w[sinatra sequel slim].each { |lib|  require lib }
+
+
+
+# CONFIG
+##############################################
+
+Sequel.connect ENV["DATABASE_URL"]
+
+# load models after connection was established
+%w[serial_number account].each { |model| require "./models/#{model}" }
+
+
+# APP
+##############################################
 
 get "/" do
   slim :index
