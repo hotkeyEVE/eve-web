4d0a0df133d314d2723dccc8298b4369616f8e15
Tom <bballts14@gmx.de>
2012-10-03 14:58:32 +0200

    Allow multiple mac addresses in accounts table.

--- db/migrations.rb
+++ db/migrations.rb
@@ -2,13 +2,14 @@ require 'sequel'
 
 DB = Sequel.connect(ENV["DATABASE_URL"])
 
-# Create accounts table
+# accounts table
+DB.drop_table :accounts
 DB.create_table :accounts do
   primary_key :id
 
   String :email, size: 100
   String :serial, size: 50
-  String :mac_id, size: 50
+  String :mac_addresses, size: 150
 
   index :serial, unique: true
 end
