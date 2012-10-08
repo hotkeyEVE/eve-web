914d681747f637d5b9864f7d271c6884af49bcc9
Tom <bballts14@gmx.de>
2012-10-08 21:51:17 +0200

    Add jQuery scrollTo lib.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,5 +3,16 @@ $(function() {
     $(this).hide();
     $(".older-versions").show();
   });
+
+  $(".app-shortcuts a").click(function(event) {
+    event.preventDefault();
+
+    $(".app-shortcuts table").hide();
+    $(".app-shortcuts").removeClass("active");
+
+    $(this).siblings("table").show();
+    $(this).parent().addClass("active");
+    $(this).parent().ScrollTo();
+  });
 });
 
