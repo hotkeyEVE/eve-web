b7e16592bf87f60317b0f91e54ff88cc6e506876
Tobias Sommer <togo210@googlemail.com>
2012-10-23 17:15:40 +0200

    skype standard app

--- models/shortcut.rb
+++ models/shortcut.rb
@@ -2,7 +2,7 @@ class Shortcut < Sequel::Model(:menu_bar_shortcuts)
   STANDARD_APPS = %w[
     Preview Finder Reminders Terminal Xcode Mail
     Console Chrome App\ Store System\ Preferences
-    Activity\ Monitor \ Skype
+    Activity\ Monitor Skype
   ]
 end
 
