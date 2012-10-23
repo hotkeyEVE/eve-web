392782afe913c7fd894d6a70984c874ca19ef37c
Tobias Sommer <togo210@googlemail.com>
2012-10-23 17:24:34 +0200

    new standard apps

--- models/shortcut.rb
+++ models/shortcut.rb
@@ -2,7 +2,8 @@ class Shortcut < Sequel::Model(:menu_bar_shortcuts)
   STANDARD_APPS = %w[
     Preview Finder Reminders Terminal Xcode Mail
     Console Chrome App\ Store System\ Preferences
-    Activity\ Monitor Skype
+    Activity\ Monitor Skype Text Calendar Base MacVim
+    Pages Disk\ Utility Reminders VP-UML Safari GitX
   ]
 end
 
